1b05f9e5b58c01862bd2b3e75dc5db729723a76e
saleh <saleh.saiid@gmail.com>
2014-02-26 00:46:18 +0200

    add range/resume source.

--- index.php
+++ index.php
@@ -9,18 +9,57 @@ function get_apache_headers(){
 	return $headers;
 }
 
+list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+
 // Define file
 $filename = 'test.mov';
+
+if(!file_exists($filename)){
+	Header("HTTP/1.1 400 Not Found");
+	exit();
+}
 $filesize = filesize($filename); // Bytes
+$mime_type = mime_content_type($filename);
+$offset = 0;
+$limit = $filesize;
+
 
 $headers = get_apache_headers();
 
-
+Header("Accept-Ranges: 0-$filesize");
 
 if(isset($headers['Range'])){
 	$range = $headers['Range'];
 
 	// Get $offset and $limit bytes
 	list($offset, $limit) = explode('-', substr($range, 6));
+
+	if($offset == ''){
+		$offset = 0;
+	}
+
+	if($limit == ''){
+		$limit = $filesize;
+	}
+
+	if($limit > $filesize){
+		$limit = $filesize;
+	}
+
+	Header('HTTP/1.0 206 Partial Content');
 }
+
+$content_length = $limit - $offset;
+
+Header("Content-Type: $mime_type");
+Header("Content-Length: $content_length");
+Header("Content-Range: bytes $offset-$limit/$filesize");
+
+$handle = fopen($filename, 'rb');
+
+echo stream_get_contents($handle, $content_length, $offset);
+
+fclose($handle);
+
+exit();
 ?>
